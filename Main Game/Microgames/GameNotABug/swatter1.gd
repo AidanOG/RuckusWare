c2478f51def53964c1f569ac41447481f07cf7b2
Aidan O'Gorman <62823756+AidanOG@users.noreply.github.com>
2024-11-19 23:37:56 -0500

    added screen boundary collisions and swing cooldown logic for not a bug game

--- Main Game/Microgames/GameNotABug/swatter1.gd
+++ Main Game/Microgames/GameNotABug/swatter1.gd
@@ -1,17 +1,24 @@
 extends CharacterBody2D
 
+@export var swat_cooldown_timer: Timer
 
 var SPEED = 1000.0 * GameManager.game_speed
 
 
 func _physics_process(delta):
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("move_left_0", "move_right_0", "move_up_0", "move_down_0")
-	if direction:
-		velocity = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	if swat_cooldown_timer.get_time_left() <= 0:
+		
+		if Input.is_action_just_pressed("any_button_0"):
+			swat_cooldown_timer.wait_time = 0.5/GameManager.game_speed
+			swat_cooldown_timer.start()
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Input.get_vector("move_left_0", "move_right_0", "move_up_0", "move_down_0")
+		if direction:
+			velocity = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.y = move_toward(velocity.y, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
+		
